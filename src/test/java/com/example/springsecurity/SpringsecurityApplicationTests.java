96c514364ee8d4ec490a93acbe4976536997e6af
Sachin Liyanage <59449070+Sachin-Liyanage@users.noreply.github.com>
2020-07-07 23:57:07 +0530

    Update SpringsecurityApplicationTests.java

--- src/test/java/com/example/springsecurity/SpringsecurityApplicationTests.java
+++ src/test/java/com/example/springsecurity/SpringsecurityApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//10MKBThX6gKoqwmZmVB4YfX1qMGTz-mdB
 @SpringBootTest
 class SpringsecurityApplicationTests {
 
