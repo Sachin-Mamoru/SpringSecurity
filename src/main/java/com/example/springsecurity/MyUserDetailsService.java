659ef91fdfb3c5af5aa914d6c1444a90463592e1
Sachin Liyanage <59449070+Sachin-Liyanage@users.noreply.github.com>
2021-09-24 20:38:35 +0530

    Update MyUserDetailsService.java

--- src/main/java/com/example/springsecurity/MyUserDetailsService.java
+++ src/main/java/com/example/springsecurity/MyUserDetailsService.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+//Service
 @Service
 public class MyUserDetailsService implements UserDetailsService {
     @Override
